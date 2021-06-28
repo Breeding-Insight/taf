8fd54a7e986b32fe814b9f2b4544c78473113802
HMS17 <84345306+HMS17@users.noreply.github.com>
2021-06-28 09:34:24 -0400

    Saving draft

--- populate-taf-data.sql
+++ populate-taf-data.sql
@@ -24,32 +24,41 @@ DECLARE
     user_id UUID;
 BEGIN
 
-user_id := (SELECT id FROM bi_user WHERE name = 'system');
+by_user_id := (SELECT id FROM bi_user WHERE name = 'system');
 
 --Populate Users
 INSERT INTO bi_user (orcid, name, email, created_by, updated_by, active) 
 VALUES 
-('0000-0001-5398-6158', 'Christian', 'christian@mailinator.com', user_id, user_id, true);
+('0000-0001-5398-6158', 'Christian', 'christian@mailinator.com', by_user_id, by_user_id, true);
 
 INSERT INTO bi_user (orcid, name, email, created_by, updated_by, active)
 VALUES
-('0000-0001-7266-4760', 'Cucumber Breeder', 'cucumberbreeder@mailinator.com', user_id, user_id, true);
+('0000-0001-7266-4760', 'Cucumber Breeder', 'cucumberbreeder@mailinator.com', by_user_id, by_user_id, true);
 
 INSERT INTO bi_user (orcid, name, email, created_by, updated_by, active) 
 VALUES
-('0000-0002-9869-7322', 'Cucumber Member', 'cucumbermember@mailinator.com', user_id, user_id, true);
+('0000-0002-9869-7322', 'Cucumber Member', 'cucumbermember@mailinator.com', by_user_id, by_user_id, true);
 
 INSERT INTO bi_user (orcid, name, email, created_by, updated_by, active)
 VALUES 
-('0000-0002-7046-0251', 'TrailMix Breeder', 'trailmix@mailinator.com', user_id, user_id, true);
+('0000-0002-7046-0251', 'TrailMix Breeder', 'trailmix@mailinator.com', by_user_id, by_user_id, true);
 
 INSERT INTO system_user_role (bi_user_id, system_role_id, created_by, updated_by) 
-SELECT bi_user.id, system_role.id, user_id, user_id FROM bi_user JOIN system_role ON bi_user.name = 'Christian' and system_role.domain = 'admin';
+SELECT bi_user.id, system_role.id, by_user_id, by_user_id FROM bi_user JOIN system_role ON bi_user.name = 'Christian' and system_role.domain = 'admin';
 
 --Populate Programs
---INSERT INTO table (name, active) VALUES ()
+INSERT INTO program (species_id, name, created_by, updated_by, active) 
+SELECT species.id, 'Cucumber', by_user_id, by_user_id, true FROM species WHERE species.common_name = '';
+INSERT INTO program (species_id, name, created_by, updated_by, active) 
+SELECT species.id, 'Trail Mix', by_user_id, by_user_id, true FROM species WHERE species.common_name = '';
+INSERT INTO program (species_id, name, created_by, updated_by, active) 
+SELECT species.id, 'Snacks', by_user_id, by_user_id, true FROM species WHERE species.common_name = '';
 
 --Add Users To Programs
+--INSERT INTO program_user_role (program_id, user_id, role_id, created_by, updated_by, active) 
+--SELECT program.id, bi_user.id, role.id, by_user_id, by_user_id, true FROM bi_user JOIN role ON bi_user.name = '' and role.domain = 'breeder'
+--JOIN program ON program.name = '';
+--way to do this without so many joins?
 
 
 END $$;
