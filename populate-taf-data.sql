4de3322ac6ce0f2e8018f11619c91364c08f68c8
HMS17 <84345306+HMS17@users.noreply.github.com>
2021-06-28 14:58:17 -0400

    Updating user roles

--- populate-taf-data.sql
+++ populate-taf-data.sql
@@ -58,19 +58,25 @@ SELECT species.id, 'Snacks', by_user_id, by_user_id, true FROM species WHERE spe
 INSERT INTO program_user_role (program_id, user_id, role_id, created_by, updated_by, active) 
 SELECT program.id, bi_user.id, role.id, by_user_id, by_user_id, true FROM bi_user JOIN role ON bi_user.name = 'Cucumber Breeder' and role.domain = 'breeder'
 JOIN program ON program.name = 'Snacks';
+
 INSERT INTO program_user_role (program_id, user_id, role_id, created_by, updated_by, active) 
 SELECT program.id, bi_user.id, role.id, by_user_id, by_user_id, true FROM bi_user JOIN role ON bi_user.name = 'Cucumber Breeder' and role.domain = 'member'
 JOIN program ON program.name = 'Trail Mix';
+
+INSERT INTO program_user_role (program_id, user_id, role_id, created_by, updated_by, active) 
+SELECT program.id, bi_user.id, role.id, by_user_id, by_user_id, true FROM bi_user JOIN role ON bi_user.name = 'Cucumber Member' and role.domain = 'member'
+JOIN program ON program.name = 'Snacks';
+
 INSERT INTO program_user_role (program_id, user_id, role_id, created_by, updated_by, active) 
 SELECT program.id, bi_user.id, role.id, by_user_id, by_user_id, true FROM bi_user JOIN role ON bi_user.name = 'TrailMix Breeder' and role.domain = 'breeder'
-JOIN program ON program.name = 'Trail Mix';INSERT INTO program_user_role (program_id, user_id, role_id, created_by, updated_by, active) 
-SELECT program.id, bi_user.id, role.id, by_user_id, by_user_id, true FROM bi_user JOIN role ON bi_user.name = 'Christian' and role.domain = 'breeder'
-JOIN program ON program.name = 'Snacks';
-INSERT INTO program_user_role (program_id, user_id, role_id, created_by, updated_by, active) 
-SELECT program.id, bi_user.id, role.id, by_user_id, by_user_id, true FROM bi_user JOIN role ON bi_user.name = 'Christian' and role.domain = 'member'
 JOIN program ON program.name = 'Trail Mix';
 
---way to do this without so many joins?
+INSERT INTO program_user_role (program_id, user_id, role_id, created_by, updated_by, active) 
+SELECT program.id, bi_user.id, role.id, by_user_id, by_user_id, true FROM bi_user JOIN role ON bi_user.name = 'Christian' and role.domain = 'breeder'
+JOIN program ON program.name = 'Snacks';
 
+INSERT INTO program_user_role (program_id, user_id, role_id, created_by, updated_by, active) 
+SELECT program.id, bi_user.id, role.id, by_user_id, by_user_id, true FROM bi_user JOIN role ON bi_user.name = 'Christian' and role.domain = 'breeder'
+JOIN program ON program.name = 'Trail Mix';
 
 END $$;
