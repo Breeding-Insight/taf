b5c49940a66f757695e5b0f9e3d3aef9c00b8554
HMS17 <84345306+HMS17@users.noreply.github.com>
2021-06-30 11:47:08 -0400

    Another attempt at screenshot
    this.attach key, just a matter of taking screenshot to pass into it

--- src/step_definitions/steps.js
+++ src/step_definitions/steps.js
@@ -101,12 +101,17 @@ Given(/^user logs in as "([^"]*)"$/, async (args1) => {
   }
   await page.setValue("@emailInput", email);
   await page.setValue("@passwordInput", password);
-  await page.click("@signInButton");
-  await client.takeScreenshot().then(function (buffer) {
-	  return this.attach(buffer, 'image/png')});
-	//need driver defined
+  await page.click("@signInButton").saveScreenshot(`./screenshots/program-selecton.png`);
+  this.attach(fs.readFileSync(`./screenshots/program-selecton.png`), 'image/png');
+  //await client.takeScreenshot().then(function (buffer) {
+	  //return this.attach(buffer, 'image/png')});
+	//need driver/browser defined
+	//browser and client in theory interchangeable but client doesn't have takeScreenshot()?
 	//also asynchronous, need to wait for it, await?
+	//but client can ALSO equal nightwatch?
   //.saveScreenshot(`./screenshots/program-selecton.png`);
+	
+	//fs.readFileSync(file)
 });
 
 When(/user selects "([^"]*)" on program-selection page$/, async (args1) => {
