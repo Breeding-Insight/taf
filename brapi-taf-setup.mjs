56b601badf9f9b31b1b5defad4f2fb68709b179c
HMS17 <84345306+HMS17@users.noreply.github.com>
2021-07-14 17:58:12 -0400

    fixing import

--- brapi-taf-setup.mjs
+++ brapi-taf-setup.mjs
@@ -1,42 +1,7 @@
 import axios from 'axios';
-//import pg from 'pg';
-//const { Client } =  pg;
-//Uncertain about host and port
-//or it could be a # or _ in the password supposedly?
-/*
-const pgClient = new pg.Client({
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-await pgClient.connect();
+import core from '@actions/core';
 
-let res = await pgClient.query("SELECT id FROM program WHERE name='Snacks' AS snacksId", (err, res) => {
-  if (err) {
-    console.log(err.stack)
-  } else {
-    console.log(res.rows[0])
-  }
-});
-
-let snacksId = res.rows[0].snacksId;
-await pgClient.end();
-*/
-
-//json retrieval 
-/*import fs from 'fs';
-fs.readFile('./programIds.json', 'utf8', (err, jsonString) => {
-  if (err) {
-    console.log("File read failed:", err);
-    return;
-  }
-  console.log("File data:", jsonString);
-  let programInfo = JSON.parse(data);
-})*/
-
-const core = require('@actions/core');
+#const core = require('@actions/core');
 
 core.info(process.argv);
 let programInfo = process.argv.slice(2);
