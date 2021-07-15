7b686e719cb43325c0c5c1be6c79f824bf28e3a8
HMS17 <84345306+HMS17@users.noreply.github.com>
2021-07-15 12:39:45 -0400

    attempting localhost

--- brapi-taf-setup.mjs
+++ brapi-taf-setup.mjs
@@ -4,12 +4,16 @@ import * as core from '@actions/core';
 core.warning(`${process.argv}  warning test`);
 core.info(`${process.argv}  info test`);
 let programInfo = process.argv.slice(2).map(str => str.trim());
-//arr = arr.filter(entry => entry.trim() != '');
 
-//axios.all( []); for calling multiple post
+/*await axios.all([
+  axios.post
+  axios.post
+]); for calling multiple post
 //make a loop
+//`${process.env.VUE_APP_BI_API_V1_PATH}/programs/${programId}/brapi/v2/search/studies`
+*/
 
-await axios.post('http://brapiserver:8080/brapi/v2/programs', {
+await axios.post('http://localhost:8080/brapi/v2/programs', {
     "programName": "Snacks",
     "externalReferences": [
         {
