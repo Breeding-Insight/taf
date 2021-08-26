9bf3f2216b589369ca51a56c7ecd8ab3215b1f20
Chris Vacalares <chris.vacalares@gmail.com>
2021-08-25 23:36:38 -0700

    Rebase from main.

--- cucumber.conf.js
+++ cucumber.conf.js
@@ -1,12 +1,18 @@
 const fs = require("fs");
-const { setDefaultTimeout, After, AfterAll, BeforeAll, Before } = require("cucumber");
+const {
+  setDefaultTimeout,
+  After,
+  AfterAll,
+  BeforeAll,
+  Before,
+} = require("@cucumber/cucumber");
 const {
   createSession,
   closeSession,
   startWebDriver,
   stopWebDriver,
   getNewScreenshots,
-  saveScreenshot
+  saveScreenshot,
 } = require("nightwatch-api");
 const reporter = require("cucumber-html-reporter");
 const { client } = require("nightwatch-api");
@@ -14,15 +20,13 @@ const { client } = require("nightwatch-api");
 setDefaultTimeout(300000);
 global.__basedir = __dirname;
 
-Before(async()=>{
-  await createSession();
-  await client.resizeWindow(1900,1200);
-})
+Before(async function () {
+  await createSession({ env: this.parameters.browser });
+  await client.maximizeWindow();
+});
 
-BeforeAll(async () => {
-  await startWebDriver({
-    env: process.argv[process.argv.indexOf("--env") + 1]
-  });
+BeforeAll(async function () {
+  await startWebDriver();
 });
 
 AfterAll(async () => {
@@ -66,7 +70,9 @@ AfterAll(async () => {
 });
 
 After(function () {
-  getNewScreenshots().forEach(file => this.attach(fs.readFileSync(file), 'image/png'));
+  getNewScreenshots().forEach((file) =>
+    this.attach(fs.readFileSync(file), "image/png")
+  );
   //Note: The following line can be commented out to keep browsers open for debugging purposes on local
   closeSession();
 });
