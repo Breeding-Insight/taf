357bdc6f963b43ba0401bb74bb53a42351ddc575
Chris Vacalares <chris.vacalares@gmail.com>
2021-11-11 08:34:11 -0800

    Set process exit 1 when test failure occurs

--- reporter.js
+++ reporter.js
@@ -18,14 +18,13 @@ try {
   reporter.generate(options);
 } catch (err) {
   console.log(err);
-  return 1;
+  process.exit(1);
 }
 
 fs.readFile("report/cucumber_report.json", function (err, data) {
   if (err) throw err;
   if (data.includes(`"status": "failed"`)) {
     console.log("Test failed.");
-    return 1;
+    process.exit(1);
   }
-  return 0;
 });
