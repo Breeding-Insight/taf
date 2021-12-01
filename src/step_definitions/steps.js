89eeb06f1d4db23f48e64ac7614e1f2b70b42fb8
Chris Vacalares <chris.vacalares@gmail.com>
2021-11-30 23:44:15 -0800

    Add tests for BI-901, BI-902, BI-903, BI-904.

--- src/step_definitions/steps.js
+++ src/step_definitions/steps.js
@@ -833,7 +833,7 @@ Then(/^user can see 'Import' button$/, async () => {
 When(/^user selects "([^"]*)" button$/, async (args1) => {
   await page.pause(1000);
   const selector = {
-    selector: `//button[starts-with(normalize-space(.),'${args1}')]`,
+    selector: `//button[starts-with(normalize-space(.),'${args1}')] | //button//span[normalize-space(.)='${args1}']`,
     locateStrategy: "xpath",
   };
   await page.waitForElementVisible(selector);
--- src/step_definitions/userManagementMenuSteps.js
+++ src/step_definitions/userManagementMenuSteps.js
@@ -18,3 +18,29 @@ Then(/^user can see a Log out button$/, async () => {
   await page.assert.visible("@logoutButton");
 });
 
+When(/^user selects "([^"]*)" of Name "([^"]*)"$/, async (args1, args2) => {
+  await page.click({
+    selector: `//table//td[@data-label='Name'][normalize-space(.)="${args2}"]/parent::tr//a[normalize-space(.)='${args1}']`,
+    locateStrategy: "xpath",
+  });
+});
+
+When(/^user selects "([^"]*)" in Role dropdown$/, async (args1) => {
+  await page.setValue(
+    { selector: "//*[@id='Role']", locateStrategy: "xpath" },
+    args1
+  );
+});
+
+Then(
+  /^user can see Name "([^"]*)" with Role as "([^"]*)"$/,
+  async (args1, args2) => {
+    await page.assert.containsText(
+      {
+        selector: `//table//td[@data-label='Name'][normalize-space(.)='${args1}']/parent::tr//td[@data-label='Role']`,
+        locateStrategy: "xpath",
+      },
+      args2
+    );
+  }
+);
