e0b8a7301946024275135bb712d1b511fcc7f1ca
Chris Vacalares <chris.vacalares@gmail.com>
2022-09-26 22:35:06 -0700

    Fixed setting of program name.

--- src/step_definitions/programManagementSteps.js
+++ src/step_definitions/programManagementSteps.js
@@ -26,9 +26,11 @@ When(/^user selects 'New Program' button in Programs page$/, async () => {
 When(
   /^user sets "([^"]*)" in Program Name field in Programs page$/,
   async function (args1) {
-    program.Name = args1.replace("*", helpers.generateRandomAlphaString(5));
     await page.section.programForm.clearValue("@programNameField");
-    await page.section.programForm.setValue("@programNameField", program.Name);
+    await page.section.programForm.setValue(
+      "@programNameField",
+      args1.replace("*", helpers.generateRandomAlphaString(5))
+    );
   }
 );
 
@@ -71,19 +73,22 @@ Then(
   }
 );
 
-When(/^user selects 'Edit' of "([^"]*)" in Programs page$/, async (args1) => {
-  let programName;
-  if (program.Name != null) {
-    programName = program.Name;
-  } else {
-    programName = args1;
+When(
+  /^user selects 'Edit' of "([^"]*)" in Programs page$/,
+  async function (args1) {
+    let programName;
+    if (args1.includes("*")) {
+      programName = program.Name;
+    } else {
+      programName = args1;
+    }
+    const selector = {
+      selector: `.//td[@data-label='Name'][normalize-space(.)='${programName}']/ancestor::tr//td/a[normalize-space(.)='Edit']`,
+      locateStrategy: "xpath",
+    };
+    await page.click(selector);
   }
-  const selector = {
-    selector: `.//td[@data-label='Name'][normalize-space(.)='${programName}']/ancestor::tr//td/a[normalize-space(.)='Edit']`,
-    locateStrategy: "xpath",
-  };
-  await page.click(selector);
-});
+);
 
 Then(
   /^user can see "([^"]*)" in Program Name field in Programs page$/,
@@ -110,9 +115,12 @@ Then(
   /^user can see "([^"]*)" in Name column in Program page$/,
   async (args1) => {
     //will find match on 1st row only
-    if (program.Name == null) {
-      await page.section.programForm.isItemInRow({ Name: args1 });
-    } else await page.section.programForm.isItemInRow({ Name: program.Name });
+    if (args1.includes("*")) {
+      programName = program.Name;
+    } else {
+      programName = args1;
+    }
+    await page.section.programForm.isItemInRow({ Name: programName });
   }
 );
 Then(
