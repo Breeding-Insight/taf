dea2eccafcfdf273f79952048b7053e564c0dac1
cvacalares <chris.vacalares@gmail.com>
2025-04-06 16:50:27 -0700

    Disable to workers flag.

--- cucumber.conf.js
+++ cucumber.conf.js
@@ -1,91 +1,98 @@
-const Nightwatch = require('nightwatch');
-const {After, AfterAll, Before, setDefaultTimeout} = require('@cucumber/cucumber');
+const Nightwatch = require("nightwatch");
+const {
+  After,
+  AfterAll,
+  Before,
+  setDefaultTimeout,
+} = require("@cucumber/cucumber");
 
 setDefaultTimeout(-1);
 global.__basedir = __dirname;
 
-Before(function({pickle}) {
+Before(function ({ pickle }) {
   const webdriver = {};
 
-  if (this.parameters['webdriver-host']) {
-    webdriver.host = this.parameters['webdriver-host'];
+  if (this.parameters["webdriver-host"]) {
+    webdriver.host = this.parameters["webdriver-host"];
   }
 
-  if (this.parameters['webdriver-port']) {
-    webdriver.port = this.parameters['webdriver-port'];
+  if (this.parameters["webdriver-port"]) {
+    webdriver.port = this.parameters["webdriver-port"];
   }
 
-  if (typeof this.parameters['start-process'] != 'undefined') {
-    webdriver.start_process = this.parameters['start-process'];
+  if (typeof this.parameters["start-process"] != "undefined") {
+    webdriver.start_process = this.parameters["start-process"];
   }
 
   let persist_globals;
-  if (this.parameters['persist-globals']) {
-    persist_globals = this.parameters['persist-globals'];
+  if (this.parameters["persist-globals"]) {
+    persist_globals = this.parameters["persist-globals"];
   }
 
   const globals = {};
-  if (this.parameters['retry-interval']) {
-    globals.waitForConditionPollInterval = this.parameters['retry-interval'];
+  if (this.parameters["retry-interval"]) {
+    globals.waitForConditionPollInterval = this.parameters["retry-interval"];
   }
 
   this.client = Nightwatch.createClient({
-    headless: this.parameters.headless,
-    env: this.parameters.env,
+    // headless: this.parameters.headless,
+    // env: this.parameters.env,
     timeout: this.parameters.timeout,
     parallel: !!this.parameters.parallel,
-    output: !this.parameters['disable-output'],
+    // output: !this.parameters["disable-output"],
     enable_global_apis: true,
-    silent: !this.parameters.verbose,
+    // silent: !this.parameters.verbose,
     always_async_commands: true,
     webdriver,
     persist_globals,
     config: this.parameters.config,
-    globals
+    globals,
   });
 
   if (this.client.settings.sync_test_names) {
-    const {name} = pickle;
+    const { name } = pickle;
     this.client.updateCapabilities({
-      name
+      name,
     });
   }
 
-  const {options = {}} = this.client.settings.test_runner;
+  const { options = {} } = this.client.settings.test_runner;
 
   // auto_start_session is true by default
-  if (options.auto_start_session || typeof options.auto_start_session == 'undefined') {
-    return this.client.launchBrowser().then(browser => {
+  if (
+    options.auto_start_session ||
+    typeof options.auto_start_session == "undefined"
+  ) {
+    return this.client.launchBrowser().then((browser) => {
       this.browser = browser;
     });
   }
 });
 
 After(async function (testCase) {
-  if (testCase.result.status === 'FAILED' && this.browser) {
+  if (testCase.result.status === "FAILED" && this.browser) {
     let filename = `screenshots/${testCase.pickle.name}-${Date.now()}.png`;
     await this.browser.saveScreenshot(filename);
     const fs = require("fs");
-    this.attach(fs.readFileSync(filename), "image/png")
+    this.attach(fs.readFileSync(filename), "image/png");
   }
   if (this.browser) {
-    // await this.browser.quit();
+    await this.browser.quit();
   }
-}) 
+});
 
-
-AfterAll(async function() {
+AfterAll(async function () {
   var reporter = require("cucumber-html-reporter");
-const fs = require("fs");
+  const fs = require("fs");
 
-try {
-  let runInfo;
-  fs.readFile("run.json", (err, data) => {
-    if (err) {
-      console.log("File read failed:", err);
-      throw "Error opening file.";
-    }
-    runInfo = JSON.parse(data);
+  try {
+    let runInfo;
+    fs.readFile("run.json", (err, data) => {
+      if (err) {
+        console.log("File read failed:", err);
+        throw "Error opening file.";
+      }
+      runInfo = JSON.parse(data);
     reporter.generate({
       theme: "bootstrap",
       jsonFile: "report/cucumber_report.json",
@@ -93,24 +100,23 @@ try {
       reportSuiteAsScenarios: true,
       launchReport: true,
       metadata: {
-        "Breeding Insight": runInfo.BreedingInsight,
-        Browser: runInfo.browserName,
-        "Browser Version": runInfo.version,
-        OS: runInfo.platform,
+          "Breeding Insight": runInfo.BreedingInsight,
+          Browser: runInfo.browserName,
+          "Browser Version": runInfo.version,
+          OS: runInfo.platform,
       },
     });
-  });
-} catch (err) {
-  console.log(err);
-  process.exit(1);
-}
-
-fs.readFile("report/cucumber_report.json", function (err, data) {
-  if (err) throw err;
-  if (data.includes(`"status": "failed"`)) {
-    console.log("Test failed.");
+    });
+  } catch (err) {
+    console.log(err);
     process.exit(1);
   }
+
+  // fs.readFile("report/cucumber_report.json", function (err, data) {
+  //   if (err) throw err;
+  //   if (data.includes(`"status": "failed"`)) {
+  //     console.log("Test failed.");
+  //     process.exit(1);
+  //   }
+  // });
 });
-  
-})
--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -14,10 +14,10 @@
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: ['src/step_definitions'],
+  src_folders: ["src/step_definitions"],
 
   // See https://nightwatchjs.org/guide/concepts/page-object-model.html
-  page_objects_path: 'src/page_objects',
+  page_objects_path: "src/page_objects",
 
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
   custom_commands_path: [],
@@ -27,84 +27,76 @@ module.exports = {
 
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
   plugins: [],
-  
+
   // See https://nightwatchjs.org/guide/concepts/test-globals.html
-  globals_path: '',
-  
+  globals_path: "",
+
   webdriver: {},
 
   output_folder: "report",
 
   test_workers: {
-    enabled: true
+    enabled: false,
   },
 
   test_runner: {
     // set cucumber as the runner
     // For more info on using CucumberJS with Nightwatch, visit:
     // https://nightwatchjs.org/guide/writing-tests/using-cucumberjs.html
-    type: 'cucumber',
+    type: "cucumber",
 
     // define cucumber specific options
     options: {
       //set the feature path
-      feature_path: 'src/features/*.feature',
-      // tags: '@debug',d
+      feature_path: "src/features/*.feature",
+      // tags: '@debug'
 
       // start the webdriver session automatically (enabled by default)
       auto_start_session: true,
-      require: ['./cucumber.conf.js', './CucumberWorld.js']
+      require: ["./cucumber.conf.js"],
 
       // use parallel execution in Cucumber
       // parallel: 2 // set number of workers to use (can also be defined in the cli as --parallel 2
-    }
+    },
   },
 
   test_settings: {
     default: {
       disable_error_log: false,
-      launch_url: 'http://localhost',
+      launch_url: "http://localhost",
 
       screenshots: {
         enabled: true,
-        path: 'screens',
-        on_failure: true
+        path: "screensshots",
+        on_failure: true,
       },
 
       desiredCapabilities: {
-        browserName: 'chrome'
+        browserName: "chrome",
       },
-      
+
       webdriver: {
         start_process: true,
         server_path: require("chromedriver").path,
         port: 4444,
-        cli_args: ['--port=4444']
+        cli_args: ["--port=4444"],
       },
-        
     },
-    
+
     chrome: {
       desiredCapabilities: {
-        browserName: 'chrome',
-        'goog:chromeOptions': {
+        browserName: "chrome",
+        "goog:chromeOptions": {
           // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
           args: [
-            '--no-sandbox',
-            '--ignore-certificate-errors',
-            '--allow-insecure-localhost',
-            '--edge-skip-compat-layer-relaunch'
-          ]
-        }
+            "--no-sandbox",
+            "--ignore-certificate-errors",
+            "--allow-insecure-localhost",
+            "--edge-skip-compat-layer-relaunch",
+            "--disable-gpu",
+          ],
+        },
       },
-
-      webdriver: {
-        start_process: true,
-        server_path: '',
-        cli_args: [
-          // --verbose
-        ]
-      }
     },
   },
 };
