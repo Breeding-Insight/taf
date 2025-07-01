cbcec8e281ab6f734e660efbab22ea556af1e331
cvacalares <chris.vacalares@gmail.com>
2025-06-30 18:36:24 -0700

    Handle browser and browser capabilities are defined.

--- cucumber.conf.js
+++ cucumber.conf.js
@@ -102,7 +102,12 @@ After(async function (testCase) {
     fs.rmSync(this.tmpUserDataDir, { recursive: true, force: true });
   }
 
-  if (!this.browser?.globals?.run?.browserName) {
+  // Add guard to prevent TypeError if browser or capabilities are undefined
+  if (
+    this.browser &&
+    this.browser.capabilities &&
+    !this.browser?.globals?.run?.browserName
+  ) {
     const caps = this.browser.capabilities;
     const globalsRun = this.browser.globals.run;
 
